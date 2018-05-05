5136450f18eefbce6c59565894c27bfc01ed5834
vinay2901 <vinay2901@gmail.com>
2018-05-05 17:27:37 +0530

    structure

---
+++ api/controllers/UserController.js
@@ -0,0 +1,14 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+home:function(req,res){
+    res.send({message:"woooooww"})
+}
+};
+
